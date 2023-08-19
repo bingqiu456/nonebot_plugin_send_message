9346b4433a5cb8f442a5d9d850c2721bee0d8439
bingyue <99388013+bingqiu456@users.noreply.github.com>
2023-08-20 00:48:19 +0800

    🥕更新到`3.0`

--- nonebot_plugin_send_message/__init__.py
+++ nonebot_plugin_send_message/__init__.py
@@ -1,10 +1,25 @@
+from asyncio.log import logger
 from nonebot import on_message, on_notice, on_command
 from nonebot.matcher import Matcher
 from nonebot.adapters.onebot.v11 import PrivateMessageEvent,PRIVATE,Bot,FriendAddNoticeEvent,Message
 from nonebot.params import CommandArg,Depends
 from nonebot.permission import SUPERUSER
-from . import config
+from .config import Config
+from nonebot.plugin import PluginMetadata
 
+
+if not Config.friend_list:
+    logger.warning("未配置名单！")
+
+__plugin_meta__ = PluginMetadata(
+    name="双向聊天插件",
+    description="一个支持双向聊天的传话插件，可用于临时聊天而不想加好友场景",
+    usage="没什么用",
+    type="application",
+    config=Config,
+    homepage="https://github.com/bingqiu456/nonebot_plugin_send_message",
+    supported_adapters="onebot v11"
+)
 
 async def dependency():
     Matcher.skip()
@@ -24,10 +39,10 @@ async def _(bot:Bot,matcher: Matcher,event:PrivateMessageEvent,c:Message = Comma
 
 @js.handle()
 async def _(bot:Bot, event: PrivateMessageEvent,check=Depends(dependency)):
-    for i in config.friend_list:
+    for i in Config.friend_list:
         await bot.send_private_msg(user_id=int(i),message=f"来自好友:{event.get_user_id()}\n"+event.get_message(),auto_escape=False)
     await js.finish()
 
 @b.handle()
 async def _(event:FriendAddNoticeEvent):
-    await b.finish(Message(config.message))
+    await b.finish(Message(Config.message))
